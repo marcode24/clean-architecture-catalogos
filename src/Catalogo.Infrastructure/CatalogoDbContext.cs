10d139db91cdd5c5a40930e04eec36c570db5c20
Marco Cruz <marco24cruz08@gmail.com>
2024-10-21 13:08:07 -0600

    ✨ Refactor CatalogoDbContext to implement IUnitOfWork interface and override SaveChangesAsync method

--- src/Catalogo.Infrastructure/CatalogoDbContext.cs
+++ src/Catalogo.Infrastructure/CatalogoDbContext.cs
@@ -1,17 +1,21 @@
+using Catalogo.Domain.Abstractions;
 using Microsoft.EntityFrameworkCore;
 
 namespace Catalogo.Infrastructure;
 
-public sealed class CatalogoDbContext : DbContext
+public sealed class CatalogoDbContext : DbContext, IUnitOfWork
 {
-  public CatalogoDbContext(DbContextOptions options) : base(options)
-  {
-
-  }
+  public CatalogoDbContext(DbContextOptions options) : base(options) { }
 
   protected override void OnModelCreating(ModelBuilder modelBuilder)
   {
     modelBuilder.ApplyConfigurationsFromAssembly(typeof(CatalogoDbContext).Assembly);
     base.OnModelCreating(modelBuilder);
+  }
+
+  public override async Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+  {
+    var results = await base.SaveChangesAsync(cancellationToken);
+    return results;
   }
 }
